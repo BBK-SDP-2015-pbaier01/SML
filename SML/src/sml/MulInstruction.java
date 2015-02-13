9dd1ea8d99bc880a3ff5b4d18ea662d127553f97
Peter Baier <peter.baier@nsg-team.com>
2015-02-13 21:29:18 +0000

    Sub, Div, Mul Instructions

---
+++ SML/src/sml/MulInstruction.java
@@ -0,0 +1,22 @@
+package sml;
+
+public class MulInstruction extends Instruction {
+
+	public MulInstruction(String label, int result, int op1, int op2) {
+		super(label, "mul", result, op1, op2);
+	}
+
+	@Override
+	public void execute(Machine m) {
+		int value1 = m.getRegisters().getRegister(op1);
+		int value2 = m.getRegisters().getRegister(op2);
+		m.getRegisters().setRegister(result, value1 * value2);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " " + op1 + " - " + op2 + " to " + result;
+	}
+
+
+}
